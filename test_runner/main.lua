17a45b24f6ca9b93f3f2d0c0a448b94fa66950fb
David Besen <dbesen@gmail.com>
2026-06-30 17:06:44 -0600

    Add basic entity IDs

--- test_runner/main.lua
+++ test_runner/main.lua
@@ -1,7 +1,57 @@
 lu = require('lib.luaunit.luaunit')
 
+local curr_ent_id = -1
+local is_alive = {}
+
+function createEntity()
+	curr_ent_id = curr_ent_id + 1
+	is_alive[curr_ent_id] = true
+	return curr_ent_id
+end
+
+function isAlive(ent_id)
+	return is_alive[ent_id] == true
+end
+
+function destroyEntity(ent_id)
+	is_alive[ent_id] = false
+end
+
 function testBasicPass()
     lu.assertEquals(1,1)
+end
+
+function testSingleEntityLiveness()
+	local ent_id = createEntity()
+	lu.assertTrue(isAlive(ent_id))
+	destroyEntity(ent_id)
+	lu.assertFalse(isAlive(ent_id))
+end
+
+function testTwoEntityLiveness()
+	local ent_id1 = createEntity()
+	local ent_id2 = createEntity()
+	lu.assertTrue(isAlive(ent_id1))
+	lu.assertTrue(isAlive(ent_id2))
+	destroyEntity(ent_id1)
+	lu.assertFalse(isAlive(ent_id1))
+	lu.assertTrue(isAlive(ent_id2))
+	destroyEntity(ent_id2)
+	lu.assertFalse(isAlive(ent_id1))
+	lu.assertFalse(isAlive(ent_id2))
+end
+
+function testTwoEntityLivenessOtherWay()
+	local ent_id1 = createEntity()
+	local ent_id2 = createEntity()
+	lu.assertTrue(isAlive(ent_id1))
+	lu.assertTrue(isAlive(ent_id2))
+	destroyEntity(ent_id2)
+	lu.assertTrue(isAlive(ent_id1))
+	lu.assertFalse(isAlive(ent_id2))
+	destroyEntity(ent_id1)
+	lu.assertFalse(isAlive(ent_id1))
+	lu.assertFalse(isAlive(ent_id2))
 end
 
 -- We have to have at least one command line argument for test discovery to work, because of how love2d invokes lua.
